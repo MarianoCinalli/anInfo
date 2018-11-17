6f8ec3ab1d2ebefa7cd109dfaa72d716bfe059bd
MarianoCinalli <mariano_cinalli@hotmail.com>
2018-11-17 22:23:11 +0000

    New test: Testing get all tasks and get tasks for a given user

--- features/developer.feature
+++ features/developer.feature
@@ -1,6 +1,10 @@
-Feature: time management
+Feature: See tasks
 
     Scenario: Display tasks
         Given a user requets all tasks
         Then the user sees all tasks
+
+    Scenario: Display jorge.bolco tasks
+        Given jorge.bolco requets his tasks
+        Then jorge.bolco sees his tasks
 
--- features/steps/developer.py
+++ features/steps/developer.py
@@ -1,13 +1,31 @@
 from behave import given, then
 import json
-from pprint import pprint
+
+user="jorge.bolco"
 
 @given(u'a user requets all tasks')
 def step_impl(context):
     context.response = context.client.get('/tasks')
+    # Lo que se seteo aca va a pasar al context del siguiente step del test.
     context.response.json_data = json.loads(context.response.data)
-    assert len(context.response.json_data) == 1
+    assert len(context.response.json_data) == 4
 
 @then(u'the user sees all tasks')
 def step_impl(context):
     assert context.response.json_data[0]['description'] == 'Agregar icono con la imagen del usuario logeado'
+    assert context.response.json_data[1]['description'] == 'Agregar ayuda que diga como agregar nuevo candidato'
+    assert context.response.json_data[2]['description'] == 'Arreglar el bug que evita agregar candidatos'
+    assert context.response.json_data[3]['description'] == 'Agregar importar desde PC'
+
+@given(u'{user} requets his tasks')
+def step_impl(context, loggedUser):
+    context.response = context.client.get('/tasks', data=dict(user=loggedUser))
+    context.response.json_data = json.loads(context.response.data)
+    assert len(context.response.json_data) == 3
+
+@then(u'{user} sees his tasks')
+def step_impl(context):
+    assert context.response.json_data[0]['description'] == 'Agregar icono con la imagen del usuario logeado'
+    assert context.response.json_data[1]['description'] == 'Agregar ayuda que diga como agregar nuevo candidato'
+    assert context.response.json_data[2]['description'] == 'Arreglar el bug que evita agregar candidatos'
+
