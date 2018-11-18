8a6a3ac0d734c18363a0861e33a48f2a10465584
MarianoCinalli <mariano_cinalli@hotmail.com>
2018-11-18 19:07:57 +0000

    Adding test that increases time for finished task. Failing.

--- features/steps/time_tracking.py
+++ features/steps/time_tracking.py
@@ -2,6 +2,8 @@ from behave import given, when, then
 from pprint import pprint
 import json
 
+# Scenario: Add hours for task
+
 @given(u'a task')
 def step_impl(context):
     context.response = context.client.get("/task/1")
@@ -20,3 +22,22 @@ def step_impl(context):
     task = json.loads(response.data)
     assert task["timeElapsed"] == 3
 
+# Scenario: Add hours to finished task
+
+@given(u'a task with status done')
+def step_impl(context):
+    context.response = context.client.get("/task/0")
+    context.response.json_task_before = json.loads(context.response.data)
+    assert context.response.json_task_before["status"] == "done"
+    assert context.response.json_task_before["timeElapsed"] == 5
+
+@when(u'trying to add an hour to a finished task')
+def step_impl(context):
+    response = context.client.post("/task/" + str(context.response.json_task_before["id"]), data=dict(hours=1))
+    assert response.status_code == 500
+
+@then(u'the time should remain the same')
+def step_impl(context):
+    response = context.client.get("/task/" + str(context.response.json_task_before["id"]))
+    task = json.loads(response.data)
+    assert task["timeElapsed"] == context.response.json_task_before["timeElapsed"] 
--- features/time_tracking.feature
+++ features/time_tracking.feature
@@ -4,3 +4,9 @@ Feature: Time tracking
         Given a task
         When one hour is added to the task
         Then the count should increase by one
+
+   Scenario: Add hours to finished task
+        Given a task with status done
+        When trying to add an hour to a finished task
+	Then the time should remain the same
+
