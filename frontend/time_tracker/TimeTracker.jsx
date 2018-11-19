62dcb7d5314f59da69a8a7b1cc327b45efef18c1
Marcos Vrljicak <marcos.vrljicak@avature.net>
2018-11-18 21:23:06 -0300

    Dumb view

--- frontend/time_tracker/TimeTracker.jsx
+++ frontend/time_tracker/TimeTracker.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import '../node_modules/semantic-ui-css/semantic.min.css';
 
-import { Message, Container, Divider } from 'semantic-ui-react'
+import { Header, Label, Container, Divider, Segment, Breadcrumb, Search, Table, Form, Dropdown, Select, Button } from 'semantic-ui-react'
 
 export default class TimeTracker extends RootComponent {
     constructor(props) {
@@ -13,11 +13,68 @@ export default class TimeTracker extends RootComponent {
         }
     }
 
+    taskTable = () => {
+        return (
+            <Table celled>
+              <Table.Header>
+                <Table.Row>
+                  <Table.HeaderCell>Nombre</Table.HeaderCell>
+                  <Table.HeaderCell>Estimado (horas)</Table.HeaderCell>
+                  <Table.HeaderCell>Transcurrido (horas)</Table.HeaderCell>
+                  <Table.HeaderCell>Estado</Table.HeaderCell>
+                </Table.Row>
+              </Table.Header>
+
+              <Table.Body>
+                <Table.Row>
+                  <Table.Cell>Cell</Table.Cell>
+                  <Table.Cell>Cell</Table.Cell>
+                  <Table.Cell>Cell</Table.Cell>
+                  <Table.Cell>Cell</Table.Cell>
+                </Table.Row>
+                <Table.Row>
+                  <Table.Cell>Cell</Table.Cell>
+                  <Table.Cell>Cell</Table.Cell>
+                  <Table.Cell>Cell</Table.Cell>
+                  <Table.Cell>Cell</Table.Cell>
+                </Table.Row>
+              </Table.Body>
+
+            </Table>
+        )
+    }
+
+    addHours = () => {
+        return(
+            <Form>
+                <Form.Group widths='equal'>
+                <Dropdown fluid selection placeholder='Seleccionar tarea' />
+                <Form.Field inline>
+                  <Label basic color='green' pointing='right'>
+                    Horas a agregar
+                  </Label>
+                </Form.Field>
+                <Button size='small'> Agregar </Button>
+                </Form.Group>
+            </Form>
+        )
+    }
+
     render() {
         return (
-            <div>
-
-            </div>
+            <Container>
+            <Header size='huge'>PSA Spring CRM</Header>
+            <Divider />
+            <Breadcrumb>
+              <Breadcrumb.Section link>Proyectos</Breadcrumb.Section>
+              <Breadcrumb.Divider />
+              <Breadcrumb.Section link>Spring CRM</Breadcrumb.Section>
+            </Breadcrumb>
+            <Header size='medium'>PSA - Time Tracker</Header>
+            <Search />
+            {this.taskTable()}
+            {this.addHours()}
+            </Container>
         )
     }
 }
